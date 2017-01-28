888fd5e131aea34199f6462aec3ed43399844b79
Daniel Cheley <danielcheley@hotmail.com>
2017-01-28 17:39:11 -0500

    Indenting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require materialize
 
 $(document).ready(function(){
-      $('.parallax').parallax();
-      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-      $('.modal-trigger').leanModal();
-    });
+  $('.parallax').parallax();
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
