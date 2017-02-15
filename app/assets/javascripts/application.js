75bdebfa5615c5f898d305c4306ad591d83c08b9
Daniel Cheley <danielcheley@hotmail.com>
2017-02-14 22:43:22 -0500

    Deleting spaces and comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require materialize
-
 $(document).ready(function() {
   $('.parallax').parallax();
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal();
 });
